e31707c2b90c97140175fc5abb4413b59ec92eb1
Mark Tareshawty <tarebyte@gmail.com>
2013-02-27 22:23:02 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opendek::Application.config.secret_token = '03786fe45c777edbfff1666196f1e9b78be8e926dc53306cbc18346be5c3c27e14cdb5789020ae393176acaf473bdde936d675c9e92c6a606d8644267e87abed'
